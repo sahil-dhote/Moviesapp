d68222753f46445a2cc1d3e7312b07744a816864
sahil dhote <sahildhote1999@gmail.com>
2021-11-25 17:28:13 +0530

    movies app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import JsonFile from "./Database.json";
 
-function App() {
+function App()
+{
+  console.log(JsonFile);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <div className="header">
+      <h1>Movie List</h1>
     </div>
+
+      <div className="data">
+        {JsonFile.posts.map(e =>
+          <div className="moviecard">
+            <div className="card-body">
+              <img src={`${e.poster_path}`} className="card-img-top" alt="Image" />
+              <h2 className="card-title">{e.original_title}</h2>
+              <h3 className="rate">
+                Rating:- { e.rating}/10
+              </h3>
+              <h3 className="card-text"> Realease date:- {e.realease_date}</h3>
+            </div>
+            </div>
+          )}
+      </div>
+    </div>
+    
+
   );
 }
 
